28151a831324256680e454aab36decfb2b74b699
Zain <zain1pakistan9988@gmail.com>
2024-10-25 12:26:37 -0400

    The beginning

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMembers,
+        //Add more permission (intents) here
+    ]
+});
+
+//Imports dotenv variables
+require('dotenv').config();
+
+
+//Discord bot logs on
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+//Basics of having the bot message based on messages
+client.on("message", msg => {
+    if(msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+console.log(procgitess.env.TOKEN);
+
+// client.login(process.env.TOKEN)
